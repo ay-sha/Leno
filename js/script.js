423fe7685e6a2240c9cf9a987a79e0a9742f25ab
Aysha <ayshaamin680@gmail.com>
2025-07-07 13:42:45 +0600

    Home Page Done

--- js/script.js
+++ js/script.js
@@ -1,10 +1,35 @@
-// Toggle Hamburger
 document.addEventListener('DOMContentLoaded', function(){
+    // Toggle Hamburger
     const toggle = document.querySelector('.navbar__mobile-menu-toggle');
     const mobileMenu = document.querySelector('.navbar__mobile-menu-items');
     toggle.addEventListener('click', function(){
         mobileMenu.classList.toggle('active');
     });
+
+
+    // video modal
+    const modal = document.getElementById('videoModal');
+    const videoBtn = document.querySelector('.preview__video-btn');
+    const videoPlayer = document.getElementById('videoPlayer')
+    const closeBtn = document.querySelector('.modal__close-btn');
+
+    videoBtn.addEventListener('click', function(){
+        modal.style.display='block'; 
+
+        videoPlayer.src = 'https://www.youtube.com/embed/2qBiUz07Vew';
+
+    });
+    closeBtn.addEventListener('click',function(){
+        modal.style.display='none';
+        videoPlayer.src= '';
+    });
+
+    window.addEventListener('click', function(event){
+        if(event.target== modal){
+            modal.style.display='none';
+            videoPlayer.src= '';
+        }
+    })
 });
 
 // Change Navbar Background on scroll
